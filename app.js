63780c44eea88e507cd52b082939f1781ccd233f
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-07-02 23:00:29 +0200

    implemented-overall-user-functionality-and-password-hashing

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 
@@ -22,6 +24,12 @@ app.use(
             date: String!
         }
 
+		type User {
+			_id: ID!
+            email: String!
+            password: String
+        }	
+		
         input EventInput {
             title: String!
             description: String!
@@ -29,12 +37,18 @@ app.use(
             date: String!
         }
 
+		input UserInput {
+			email: String!
+			password: String!
+		}
+
         type RootQuery {
             events: [Event!]!
         }
 
         type RootMutation{
             createEvent(eventInput: EventInput): Event
+			createUser(userInput: UserInput): User
         }
 
         schema {
@@ -47,10 +61,11 @@ app.use(
 				return Event.find()
 					.then(events => {
 						return events.map(event => {
-                            return { ...event._doc, _id: event.id }; // _id: event.id <---- Not necessary||Throws an error ----> _id:event.doc._id.toString()
+							return { ...event._doc, _id: event.id }; // _id: event.id <---- Not necessary // !Throws an error----> _id: event.doc._id.toString()
+																											
 						});
 					})
-					.catch(err => { 
+					.catch(err => {
 						throw err;
 					})
 			},
@@ -59,17 +74,56 @@ app.use(
 					title: args.eventInput.title,
 					description: args.eventInput.description,
 					price: +args.eventInput.price,
-					date: new Date(args.eventInput.date)
+					date: new Date(args.eventInput.date),
+					creator: '66845f1d7d97a204bc9ee987' //* Dummy user ID for now. Replace with actual user ID when implementing authentication and authorization.
 				});
-
+				
+				let createdEvent;
 				return event
 					.save()
 					.then(result => {
-						console.log(result);
-                        return { ...result._doc }; // Throws an error ----> _id:event.doc._id.toString()
-					}).
-					catch(err => {
+						createdEvent = { ...result._doc } //!Throws an error----> _id: event.doc._id.toString()
+						return User.findById('66845f1d7d97a204bc9ee987');
+					})
+					.then(user => { 
+						if (!user) {
+							throw new Error('User not found!');
+						}
+						user.createdEvents.push(event); 
+						return user.save();
+					})
+					.then(result => { 
+						return createdEvent;
+					})
+					.catch(err => {
 						console.log(err);
+						throw err;
+					});
+			},
+			createUser: args => {
+				return User
+					.findOne({ email: args.userInput.email })
+					.then(user => {
+						if (user) {
+							throw new Error('User already exists!');
+						}
+						return bcrypt.hash(args.userInput.password, 12);
+					})
+					.then(
+						hashedPassword => {
+							const user = new User({
+								email: args.userInput.email,
+								password: hashedPassword
+							});
+							return user.save();
+						}
+					)
+					.then(
+						result => {
+							return { ...result._doc, password: null, _id: result.id }; // ----> Might throw an error
+						}
+					)
+					.catch(err => {
 						throw err;
 					});
 			}
