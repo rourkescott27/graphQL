3a1585be1cf4d4fd1d34f6f60d4cd3af187160cd
rourkescott27 <126876499+rourkescott27@users.noreply.github.com>
2024-07-02 16:20:35 +0200

    connected-to-database

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
 app.use(
-    '/graphql',
-    graphqlHttp({
-        schema: buildSchema(`
+	'/graphql',
+	graphqlHttp({
+		schema: buildSchema(`
         type Event {
             _id: ID!
             title: String!
@@ -41,23 +42,45 @@ app.use(
             mutation: RootMutation
         }
     `),
-        rootValue: {
-            events: () => {
-                return events;
-            },
-            createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: args.eventInput.date
-                }
-                events.push(event);
-                return event;
-            }
-        },
-        graphiql: true
-    }));
+		rootValue: {
+			events: () => {
+				return Event.find()
+					.then(events => {
+						return events.map(event => {
+							return { ...event._doc, _id: event.id }; // Throws an error ----> _id:event.doc._id.toString()
+						});
+					})
+					.catch(err => { 
+						throw err;
+					})
+			},
+			createEvent: (args) => {
+				const event = new Event({
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: +args.eventInput.price,
+					date: new Date(args.eventInput.date)
+				});
 
-app.listen(3000);
+				return event
+					.save()
+					.then(result => {
+						console.log(result);
+						return { ...result._doc }; //-------Could produce error------, _id: event.doc._id.toString()//
+					}).
+					catch(err => {
+						console.log(err);
+						throw err;
+					});
+			}
+		},
+		graphiql: true
+	}));
+
+mongoose
+	.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqltutorial.j0jh9pa.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=graphqlTutorial`
+	).then(() => {
+		app.listen(3000);
+	}).catch(err => {
+		console.log('Connection failed:', err);
+	});
